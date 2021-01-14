1eac469ecb220bec8ccefe9f1c7dd7df507ef9dd
Mostafa <fm_benlagha@esi.dz>
2021-01-14 17:25:48 +0100

    updated

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-const GAP = 5;
-let selectedImage = 0;
+const GAP = -10;
+let isScrolling = false;
+let timeOut;
 
 const images = new Array(5).fill(null).map((_, index) => {
   const image = createSliderImage(
     `https://picsum.photos/id/${index + 900}/400`
   );
+  image.onclick = () => {
+    isScrolling = true;
+    goToImage(selectedImage, index);
+  };
   return image;
 });
+
+let selectedImage = 0;
+images[selectedImage].classList.add('selectedImage');
+const indicatorsContainer = document.createElement('div');
+indicatorsContainer.classList.add('indicatorConatiner');
+const sliderContainer = document.querySelector('.sliderContainer');
+sliderContainer.appendChild(indicatorsContainer);
+const indicators = createIndicators();
+changeIndicator(selectedImage);
+
+slider.onscroll = (e) => {
+  if (isScrolling) return;
+  const {
+    srcElement: { scrollLeft },
+  } = e;
+  const width = images[0].clientWidth;
+  const index = Math.floor(scrollLeft / width);
+  if (index !== selectedImage) goToImage(selectedImage, index);
+};
 
 function createSliderImage(imageUrl) {
   const image = document.createElement('img');
@@ -15,8 +39,22 @@ function createSliderImage(imageUrl) {
   return image;
 }
 
+function goToImage(pre, next) {
+  isScrolling = true;
+  if (timeOut) clearTimeout(timeOut);
+  timeOut = setTimeout(() => {
+    isScrolling = false;
+  }, 1000);
+  images[pre].classList.remove('selectedImage');
+  selectedImage = next;
+  images[selectedImage].classList.add('selectedImage');
+  scrollToNextImage();
+}
+
 function nextImage() {
+  images[selectedImage].classList.remove('selectedImage');
   selectedImage = (selectedImage + 1) % images.length;
+  images[selectedImage].classList.add('selectedImage');
   scrollToNextImage();
 }
 
@@ -31,10 +69,31 @@ function checkScreenImages() {
 }
 
 function scrollToNextImage() {
-  if (checkScreenImages()) selectedImage = 0;
   slider.scrollTo({
     top: 0,
     left: selectedImage * getImageWidth(),
     behavior: 'smooth',
   });
+  changeIndicator(selectedImage);
+}
+
+function createIndicators() {
+  return images.map((_, index) => {
+    const indicator = document.createElement('span');
+    indicator.classList.add('indicator');
+    indicatorsContainer.appendChild(indicator);
+    indicator.onclick = () => {
+      goToImage(selectedImage, index);
+    };
+    return indicator;
+  });
+}
+
+function changeIndicator(page) {
+  indicators.forEach((i) => i.classList.remove('selectedIndicator'));
+  indicators[page].classList.add('selectedIndicator');
+}
+
+function scrollToFooter() {
+  document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
 }
